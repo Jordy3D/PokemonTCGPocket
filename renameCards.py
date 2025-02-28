4b64ebcf3223500077d724d816cd907af7234165
Jordy3D <fchetar@gmail.com>
2025-02-28 19:26:11 +1100

    Triumphant Light (waiting on data for new Promos)

--- renameCards.py
+++ renameCards.py
@@ -7,7 +7,7 @@ with open('data/pokemons.json') as f:
     pokemons = json.load(f)
     
     
-target_series = 'Space-Time Smackdown'
+target_series = 'Triumphant Light'
 target_series_lower = target_series.lower().replace(' ', '-')
     
 # load all the cards from images/cards/space-time-smackdown
@@ -15,7 +15,7 @@ cards = os.listdir(f'images/cards/{target_series_lower}')
 # remove all non-webp files
 cards = [card for card in cards if card.endswith('.webp')]
 
-# load the pokemon object from pokemons that has the seriesName of "Space-Time Smackdown"
+# load the pokemon object from pokemons that has the given seriesName
 series = None
 for pokemon in pokemons:
     if pokemon['seriesName'] == target_series:
@@ -28,12 +28,14 @@ else:
 # rename the cards by finding the card whose number matches the pokemon's number
 # go from 1-whatever.webp to 1-Oddish-Space-Time-Smackdown.webp
 for card in cards:
-    number = card.split('-')[0]
+    number = card.split('-')[0].split('.')[0]
+    number = number.lstrip('0')
     for pokemon in series['cards']:
         if pokemon['number'] == number:
             pokemon_name = pokemon['name']
             # replace spaces with dashes
             pokemon_name = pokemon_name.replace(' ', '-')
+            pokemon_name = pokemon_name.replace("'", '')
             
             original = f'images/cards/{target_series_lower}/{card}'
             new = f'images/cards/{target_series_lower}/{number}-{pokemon_name}-{target_series.replace(" ", "-")}.webp'
