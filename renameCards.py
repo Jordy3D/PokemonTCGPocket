3843fdad04514a27cbdd759696f25e0d5661ab5b
Jordy3D <fchetar@gmail.com>
2025-03-27 21:33:42 +1100

    Shining Revelry update

--- renameCards.py
+++ renameCards.py
@@ -29,7 +29,7 @@ target_series_lower = target_series.lower().replace(' ', '-')
 # load all the cards from images/cards/space-time-smackdown
 cards = os.listdir(f'images/cards/{target_series_lower}')
 # remove all non-webp files
-cards = [card for card in cards if card.endswith('.webp')]
+cards = [card for card in cards if card.endswith('.webp') or card.endswith('.png')]
 
 # load the pokemon object from pokemons that has the given seriesName
 series = None
@@ -56,8 +56,15 @@ for card in cards:
             original = f'images/cards/{target_series_lower}/{card}'
             new = f'images/cards/{target_series_lower}/{number}-{pokemon_name}-{target_series.replace(" ", "-")}.webp'
             
-            os.rename(original, new)
-            break
+            # if the card is already a webp file, rename it to the new name
+            if card.endswith('.webp'):
+                os.rename(original, new)
+                break
+            # if the card is a png file, convert it to a webp file using magick and then rename it
+            elif card.endswith('.png'):
+                os.system(f'magick {original} {new}')
+                os.remove(original)
+                break
     else:
         print(f'Could not find a pokemon with number {number} to rename {card}')
         sys.exit(1)
